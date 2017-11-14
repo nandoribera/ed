ecab6eeefab796880bf336c16c0dac7c21d5bd6c
nandoribera <1dam.fribera@institutoserpis.org>
2017-11-14 10:25:57 +0100

    Añadido CHolamundo con monodevelop

---
+++ CHolaMundo/CHolaMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CHolaMundo
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.Write ("Introduce tu nombre: ");
+			String nombre = Console.ReadLine ();
+			Console.WriteLine ("Hola " + nombre);
+		}
+	}
+}
