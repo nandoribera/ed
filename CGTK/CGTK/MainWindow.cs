a721aee88403e69cf449b146d5a9eda7f7dbc071
nandoribera <1dam.fribera@institutoserpis.org>
2017-11-27 13:09:49 +0100

    Subida CGTK

---
+++ CGTK/CGTK/MainWindow.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Gtk;
+
+//CLASE
+public partial class MainWindow: Gtk.Window
+{
+	//CONSTRUCTOR
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+//		for (int i = 0; i < 10; i++) {
+//			Button button = new Button ();
+//			button.Label = "Botón " + i;
+//			button.Visible = true;
+//			vbox1.Add (button);
+//		}
+//		labelSaludo.Text = "Holaaaaa";
+
+		buttonAceptar.Clicked += delegate {
+//			Button button = new Button();
+//			button.Label = "Botón creado";
+//			button.Visible = true;
+//			vbox1.Add(button);
+			labelSaludo.Text = "Hola " + entry1.Text;
+
+		};
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+}
+
+//VARIABLES
+//Clase variable
+//VBox vbox1
+//HBox hbox1
+//Label label1
+//Entry entryNombre
+//Button buttonAceptar
+//Label labelSaludo
---
+++ CGTK/CGTK/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Gtk;
+
+namespace CGTK
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow mainWindow = new MainWindow (); //OBJETO/INSTANCIA DE CLASE
+			mainWindow.Show ();
+			Application.Run ();
+		}
+	}
+}
+
+//Variable tip referencia //MainWindow tipoVariablereferenteaobjeto = new Mainwindow();
+//Variable tipo valor //int x = 7
+//CAMBIAR VARIABLES CON BOTÓN DERECHO REFACTORIZAR -> CAMBIAR NOMBRE
