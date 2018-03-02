6887b1ab880d9c67ee684267f039ab35bb55e2f8
nandoribera <1dam.fribera@institutoserpis.org>
2018-03-02 09:34:11 +0100

    Actualizados Csuma y Cvector

---
+++ CSuma/CSuma/MainWindow.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+
+		buttonSuma.Clicked += delegate {
+			int numero1 = int.Parse(num1.Text);
+			int numero2 = int.Parse(num2.Text);
+			int resultado = numero1 + numero2;
+			resultadoop.Text = resultado.ToString();
+			//Button button = new Button ();
+			//button.Visible = true;
+			//double resultado = double.Parse (num1.Text) + double.Parse (num2.Text);
+			//resultadoop.Text = resultado.ToString ();
+		};
+
+		buttonresta.Clicked += delegate {
+			Button button2 = new Button ();
+			button2.Visible = true;
+			double resultado1 = double.Parse (num1.Text) - double.Parse (num2.Text);
+			resultadoop.Text = resultado1.ToString ();
+		};
+
+		buttonmultiplicacion.Clicked += delegate {
+			Button button3 = new Button ();
+			button3.Visible = true;
+			double resultado3 = double.Parse (num1.Text) * double.Parse (num2.Text);
+			resultadoop.Text = resultado3.ToString ();
+
+		};
+	}
+			protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+			{
+				Application.Quit ();
+				a.RetVal = true;
+			}
+		}
+			
+//PASAR INT A STRING
+// int val; val.toString()
+
+//CONVERT._ (CONVERTIR TIPOS)
+
+//OPERADORES
+//"/" div entera div con decimales
+// 7 / 3 = 2
+// "%" div.rest (modulo)
+// 7 % 3 = 1
+
+//mostrar por consola los números de 10 en 10 hasta 90.
+// mas de una manera
+// un ciclo for
+// mas de un ciclo (for dentro otro for)
---
+++ CVector/CVector/CVector/Vector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CVector
+{
+	public class Vector
+	{
+		/// <summary>
+		/// Ordena un vector.
+		/// </summary>
+		/// <param name="v">v vector recorrido</param>
+		public static void Sort(int[] v) {
+	}
+}
+}
+
