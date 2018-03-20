3b04989ea7f5b7f8531cd87d153dac68087071b9
nandoribera <1dam.fribera@institutoserpis.org>
2018-03-20 09:06:20 +0100

    Actualizado proyecto GHolaMundo con SQLite

---
+++ GHolaMundo/src/main/java/org/institutoserpis/ed/HolaMundo.java
@@ -0,0 +1,11 @@
+package org.institutoserpis.ed;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola mundo desde eclipse con gradle");
+
+	}
+
+}
